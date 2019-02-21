92229d82ef36079c4d41dce8aedd197c28bc1ec2
selligZeittom <gilmottiez@gmail.com>
2019-02-21 09:36:29 +0100

    initial commit

---
+++ work/src/xf/port/idf-qt/xf.cpp
@@ -0,0 +1,50 @@
+#include <config/xf-config.h>
+
+#if (USE_XF_PORT_IDF_QT_XF_IMPLEMENTATION != 0)
+
+#include <QCoreApplication>
+#include "xf/interface/timeoutmanager.h"
+#include "xf/interface/resourcefactory.h"
+#include "xf/interface/dispatcher.h"
+#include "xf/xf.h"
+
+using interface::XFResourceFactory;
+
+// TODO: Implement code for XF class
+
+#endif // USE_XF_PORT_IDF_QT_XF_IMPLEMENTATION
+
+void XF::initialize(int timeInterval, int argc, char *argv[])
+{
+
+}
+
+int XF::exec()
+{
+
+}
+
+int XF::execOnce()
+{
+
+}
+
+interface::XFDispatcher *XF::getDefaultDispatcher()
+{
+
+}
+
+void XF_initialize(int timeInterval)
+{
+
+}
+
+void XF_exec()
+{
+
+}
+
+void XF_execOnce()
+{
+
+}
