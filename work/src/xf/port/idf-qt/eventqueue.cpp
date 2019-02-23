1cc31aca1126b9d5b11f504cfe7d1190528e11a6
selligZeittom <gilmottiez@gmail.com>
2019-02-23 15:17:24 +0100

    finished implementation of eventQueue class

--- work/src/xf/port/idf-qt/eventqueue.cpp
+++ work/src/xf/port/idf-qt/eventqueue.cpp
@@ -18,20 +18,35 @@ bool XFEventQueuePort::empty() const
 
 bool XFEventQueuePort::push(const XFEvent *pEvent)
 {
+    _mutex.lock(); //make sure that only one thread can access to that CS
     _queue.enqueue(pEvent);
+    _mutex.unlock();
 }
 
 const XFEvent *XFEventQueuePort::front()
 {
-
+    if(!_queue.isEmpty()) //check wether it is empty or not
+    {
+        return _queue.head(); //return the pointer on the first event
+    }
+    else
+    {
+        return NULL; //return NULL if the queue is empty
+    }
 }
 
 void XFEventQueuePort::pop()
 {
-
+    _mutex.lock(); //make sure that only one thread can access to that CS
+    if(!_queue.isEmpty()) //check wether it is empty or not
+    {
+        _queue.pop_front(); //remove the next event of the list
+    }
+    _mutex.unlock();
 }
 
 bool XFEventQueuePort::pend()
 {
-
+    while(_queue.isEmpty()){}; //wait for an event (it is blocking)
+    return true; //return true once an event came
 }
